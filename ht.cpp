bd900f0d79cd0c1f4ab5b9f4ac654a691196b471
ZapDos7 <ioanna.zapalidi@gmail.com>
2019-03-13 15:27:02 +0200

    ht implementation

--- ht.cpp
+++ ht.cpp
@@ -1,2 +1,12 @@
 #include "ht.h"
 
+
+ht_node::ht_node() {}
+
+ht_node::~ht_node() {}
+
+
+int ht_node::hash_fun(wallet w, int size) { //hash the key
+    int w1 = atoi(w.get_name());
+    return (w1 % size);
+}
--- ht.h
+++ ht.h
@@ -1,6 +1,17 @@
 #ifndef _HT_H_
 #define _HT_H_
 
+#include "wallet.h"
 
+class ht_node
+{
+private:
+    wallet myData;
+
+public:
+    ht_node();
+    ~ht_node();
+    int hash_fun(wallet w, int size);
+};
 
 #endif
